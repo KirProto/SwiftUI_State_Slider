1aab2330604979ac8bae9776263b070f26a2b636
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 10:14:10 +0500

    Initial Commit

---
+++ Yan_SwiftUI_State_Slider/Yan_SwiftUI_State_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_State_SliderApp.swift
+//  Yan_SwiftUI_State_Slider
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_State_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
